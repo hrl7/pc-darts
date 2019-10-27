35f8d6ed344efd9591e36812985230823f3e470a
zigen <zigen@horol.org>
2019-10-27 19:53:38 +0900

    add sound for a dart thrown

--- renderer/pages/Game.js
+++ renderer/pages/Game.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
+import sound from "../../assets/dart-thrown.wav";
 
+const audio = new Audio(sound);
 export const Game = (props) => {
   const  { state } = props;
+  const [score, setScore] = useState(0);
+  if (state.score !== score) {
+    setScore(state.score);
+    audio.play();
+  }
+
+
   return (<div>
     <div>
       <h1>Score: {state.score}</h1>
